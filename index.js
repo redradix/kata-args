6a46d0411132cd24b8884d75e0be51bb00a3d84a
Aarón Contreras <aaron@redradix.com>
2021-06-18 12:37:41 +0200

    Return the actual value provided to the arg

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const parseArgs = (args) => {
   if (args.includes("-l")) {
     return { l: true };
   }
-  if (args.includes("-p")) {
-    return { p: "4000" };
+  if (args.includes("-p ")) {
+    return { p: args.slice(3) };
   }
   return {};
 };
