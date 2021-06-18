b0c12ceec9048fb4521f6c279744e78ade39be9d
Roberto Redradix <roberto@redradix.com>
2021-06-18 12:47:02 +0200

    Add Prettier

--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
-const { parseArgs } = require("./index.js");
+const { parseArgs } = require('./index.js')
 
-describe("Parser", () => {
-  it("no params", () => {
-    expect(parseArgs("")).toEqual({});
-  });
+describe('Parser', () => {
+  it('no params', () => {
+    expect(parseArgs('')).toEqual({})
+  })
 
-  it("parse a single flag", () => {
-    expect(parseArgs("-l")).toEqual({ l: true });
-  });
+  it('parse a single flag', () => {
+    expect(parseArgs('-l')).toEqual({ l: true })
+  })
 
-  it("parse a single flag with a value", () => {
-    expect(parseArgs("-p 4000")).toEqual({ p: "4000" });
-    expect(parseArgs("-p 8000")).toEqual({ p: "8000" });
-  });
+  it('parse a single flag with a value', () => {
+    expect(parseArgs('-p 4000')).toEqual({ p: '4000' })
+    expect(parseArgs('-p 8000')).toEqual({ p: '8000' })
+  })
 
-  it("provides a default value if none is provided", () => {
-    expect(parseArgs("-p")).toEqual({ p: "" });
-  });
+  it('provides a default value if none is provided', () => {
+    expect(parseArgs('-p')).toEqual({ p: '' })
+  })
 
-  it("provides a default value if none is provided", () => {
-    expect(parseArgs("-l -p 8000")).toEqual({ l: true, p: "8000" });
-  });
-});
+  it('provides a default value if none is provided', () => {
+    expect(parseArgs('-l -p 8000')).toEqual({ l: true, p: '8000' })
+  })
+})
