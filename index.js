651fe02da927e57505d004312a1c91b4c99a58da
Aarón Contreras <aaron@redradix.com>
2021-06-18 13:40:41 +0200

    Work with any schema

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ const createParser = schema => args => {
 
   const allowedArgs = Object.keys(schema)
 
+  const parser = {}
+
   args
     .filter(arg => arg.startsWith('-'))
     .map(arg => arg.slice(1))
     .forEach(arg => {
-      if (!allowedArgs.includes(arg)) throw new Error()
+      switch (schema[arg]) {
+        case 'boolean': {
+          parser[arg] = true
+          break
+        }
+        case 'string': {
+          let value = args[args.indexOf(`-${arg}`) + 1]
+          if (!value || value.startsWith('-')) {
+            value = ''
+          }
+          parser[arg] = value
+          break
+        }
+        case 'number': {
+          let value = args[args.indexOf(`-${arg}`) + 1]
+          if (!value || value.startsWith('-')) {
+            value = 0
+          }
+          parser[arg] = Number(value)
+          break
+        }
+        default:
+          throw new Error()
+      }
     })
 
-  const parser = {}
-
-  if (args.includes('-d')) {
-    let value = args[args.indexOf('-d') + 1]
-    if (!value || value.startsWith('-')) {
-      value = 0
-    }
-    parser['d'] = Number(value)
-  }
-  if (args.includes('-l')) {
-    parser['l'] = true
-  }
-  if (args.includes('-p')) {
-    let value = args[args.indexOf('-p') + 1]
-    if (!value || value.startsWith('-')) {
-      value = ''
-    }
-    parser['p'] = value
-  }
   return parser
 }
 
