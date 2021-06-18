bb097aea22da8c8390949b4366ec6fb09b88e155
Ines Pedraza Pino <inespedrazapino@MacBook-Pro-de-Ines.local>
2021-06-18 12:33:54 +0200

    feat: create test for flag and value

--- index.test.js
+++ index.test.js
@@ -1,12 +1,15 @@
-const { parseArgs } = require('./index.js')
+const { parseArgs } = require("./index.js");
 
+describe("Parser", () => {
+  it("no params", () => {
+    expect(parseArgs("")).toEqual({});
+  });
 
-describe('Parser', () => {
-  it('no params', () => {
-    expect(parseArgs('')).toEqual({})
-  })
+  it("parse a single flag", () => {
+    expect(parseArgs("-l")).toEqual({ l: true });
+  });
 
-  it('parse a single flag', () => {
-    expect(parseArgs('-l')).toEqual({l:true})
-  } )
-})
+  it("parse a single flag with a value", () => {
+    expect(parseArgs("-p 4000")).toEqual({ p: "4000" });
+  });
+});
