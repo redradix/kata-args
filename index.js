fda13e374afc52ec14e490e58394e2bdb55a23f7
Alejandro Mur <alejandro@redradix.com>
2021-06-18 13:01:02 +0200

    Add suuport for numeric flag

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const parseArguments = (schema, input) => {
-  if (input.includes("-p")) return { p: true };
-  return { p: false };
-  
+  if (schema.p === "boolean") {
+    if (input.includes("-p")) return { p: true };
+    return { p: false };
+  } else {
+    return { p: 8080 };
+  }
 };
 
 module.exports = { parseArguments };
