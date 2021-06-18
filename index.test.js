d011cfb17e59f2686c22ba80fabefb02494316d4
Gabriel Cuenca Moncín <gabriel@redradix.com>
2021-06-18 12:33:52 +0200

    parse p arg ok case, red

--- index.test.js
+++ index.test.js
@@ -2,7 +2,10 @@ const schema = require('./schema')
 const { parser } = require('./index.js')
 
 describe('Bla', () => {
+  it('ok', () => {
+    expect(() => parser('-p 8080', schema)).toBe(true)
+  })
   it('fails', () => {
-    expect(() => parser('-p 8080', schema)).toThrow(Error)
+    expect(() => parser('-p string', schema)).toThrow(Error)
   })
 })
