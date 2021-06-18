fd4b96eaba6a6998c5def2154696fc40bd95199d
Sonia Ruiz <yune.vk@gmail.com>
2021-06-18 12:31:19 +0200

    ✅  Pass green

--- index.test.js
+++ index.test.js
@@ -1,7 +1,30 @@
-const { bla } = require('./index.js')
+const { parseCommand } = require('./index.js')
 
-describe('Bla', () => {
-  it('fails', () => {
-    expect(1).toBe(2)
+const specs = [
+  {
+    name: 'Empty schema and value',
+    schema: {},
+    value: '',
+    result: {}
+  },
+  {
+    name: 'Boolean',
+    schema: {
+      a: 'boolean'
+    },
+    value: '-a',
+    result: { a: true }
+  },
+]
+
+/**
+
+ */
+
+describe('Validate Schema', () => {
+  specs.forEach(({name, schema, value, result}) => {
+    it(name, () => {
+      expect(parseCommand(schema, value)).toMatchObject(result)
+    })
   })
 })
