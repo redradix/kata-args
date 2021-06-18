e2cee4e2485205deb524bceaa5b3a27306671465
Aarón Contreras <aaron@redradix.com>
2021-06-18 13:09:23 +0200

    Parse numeric args

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const parseArgs = args => {
   const parser = {}
 
+  if (args.includes('-d')) {
+    const auxArgs = args.split(" ")
+    let value = auxArgs[auxArgs.indexOf("-d") + 1]
+    if(!value || value.startsWith('-')) {
+      value = 0
+    }
+    parser['d'] = Number(value)
+  }
   if (args.includes('-l')) {
    parser['l']= true
   }
