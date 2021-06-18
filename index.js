818f4738a1bcd54325b91634e3fa6806d1027338
Victor <victor@redradix.com>
2021-06-18 11:33:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const bla = () => {
+  console.log('bla')
+}
+
+module.exports = { bla }
---
+++ index.test.js
@@ -0,0 +1,7 @@
+const { bla } = require('./index.js')
+
+describe('Bla', () => {
+  it('fails', () => {
+    expect(1).toBe(2)
+  })
+})
